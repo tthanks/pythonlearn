40daf9341c4226ce09223895a9a5634ce230c37e
administrator1 <tthankstang@hotmail.com>
2020-07-24 16:19:09 +0800

    增加日期

--- venv/txt_mod.py
+++ venv/txt_mod.py
@@ -1,4 +1,7 @@
 import re
+import datetime, time
+
+now = datetime.datetime.now()  # 定义时间
 
 #with open('Original_list_file.txt', encoding='utf-8', ) as file:
 
@@ -6,21 +9,13 @@ file = open("Original_list_file.txt", "r")
 content = file.readline()
 
 while content != "":
+    today = now.strftime('%Y-%m-%d')  # 定义时间格式
     content_initial = content
-    #print(content_initial)
-    #content_process_step1 = re.compile(r'~[0-9]+\.?[0-9]+')
     content_process_step1 = re.compile(r'(?<=~).+?(?=~)')  ##匹配的字符是XX，但必须满足形式是AXXB这样的字符串
-    #contest_process_step2 = str.lstrip('~');
-    print(content_process_step1.findall(content_initial))
-    #str = content_process_step1.findall(content_initial)
-    #print(content_process_step2.findall(content_initial)
-
-    #content_spilit = content_initial.split("~")
-    #print(content_spilit)
-    # stock_price = re.compile(r' ' + str(stockcode) + ' ')
-    # print(content)
-    # regMatch = stock_price.findall(content)
-    # print(regMatch)
+    contest_process_step2 = content_process_step1.findall(content_initial)  ##匹配所有条件
+    contest_process_step3 = contest_process_step2[0:5]  ##保留前6项
+    contest_process_step3.insert(0,today)  ##在列表第一个元素中插入今天的日期
+    print (contest_process_step3)
     content = file.readline()
     # 读文本的全文并打印出来
     #i = file.read().splitlines()
